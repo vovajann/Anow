09549f6514ae828992199f8e7997985670f46882
vovajann <80536569+vovajann@users.noreply.github.com>
2022-07-01 13:56:35 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-   $('.header__burger').click(function (event) {
-      $('.header__burger, .menu, .header').toggleClass('active');
-      $('body').toggleClass('lock');
-   });
-});
